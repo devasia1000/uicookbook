c9139d728bef938b24610cd0e6dc571481a57b76
faziow14 <faziow14@users.noreply.github.com>
2014-10-18 22:41:58 -0500

    Deleted Cookie Demo Files

--- Cookie-Demo/setCookie.js
+++
@@ -1,7 +0,0 @@
-function setCookie(cookieName, cookieValue, daysToExpire)
-{
-	var d = new Date();
-	d.setTime(d.getTime() + (daysToExpire*24*60*60*1000));
-	var expires = "expires="+d.toUTCString();
-	document.cookie = cookieName + "=" + cookieValue + "; " + expires;
-}
